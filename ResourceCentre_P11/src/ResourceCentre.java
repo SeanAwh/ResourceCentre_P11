16fd3d9c747360b3aced7437214176398496b225
SeanAwh <20041162@myrp.edu.sg>
2021-08-03 11:12:19 +0800

    Sean - option 1 Refactor

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -129,10 +129,12 @@ public class ResourceCentre {
 
 		for (int i = 0; i < camcorderList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", camcorderList.get(i).getAssetTag(),
-					camcorderList.get(i).getDescription(), 
-					ResourceCentre.showAvailability(camcorderList.get(i).getIsAvailable()),
-					camcorderList.get(i).getDueDate(),camcorderList.get(i).getOpticalZoom());
+			String assetTagCC = camcorderList.get(i).getAssetTag();
+			String descriptionCC = camcorderList.get(i).getDescription();
+			String showAvailabilityCC = ResourceCentre.showAvailability(camcorderList.get(i).getIsAvailable());
+			String dueDateCC = camcorderList.get(i).getDueDate();
+			int opticalZoomCC = camcorderList.get(i).getOpticalZoom();
+			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", assetTagCC,descriptionCC, showAvailabilityCC,dueDateCC,opticalZoomCC);
 		}
 		return output;
 	}
@@ -149,10 +151,12 @@ public class ResourceCentre {
 		// write your code here
 		for (int i = 0; i < chromebookList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
-					chromebookList.get(i).getDescription(), 
-					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
+			String assetTagCB = chromebookList.get(i).getAssetTag();
+			String descriptionCB = chromebookList.get(i).getDescription();
+			String showAvailabilityCB = ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable());
+			String dueDateCB = chromebookList.get(i).getDueDate();
+			String osCB = chromebookList.get(i).getOs();
+			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", assetTagCB,descriptionCB, showAvailabilityCB,dueDateCB,osCB);
 		}
 		return output;
 	}
